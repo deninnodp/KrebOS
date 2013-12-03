94ba96e6e1ddbe9bfc5ab722b213a913eac7080a
David DeNinno <deninnodp@gmail.com>
2013-12-02 22:10:44 -0500

    got local storage working. Took a long time, and for some reason the JS console doesn't report ANY errors that have to do with localStorage, so that's fun. Right now format is totally done, and actually creates the local storage properly. The file system view now also shows properly. For some reason, it REALLY doesn't like having a width lower than the text overflow, even when overflow is set to scroll or auto. It still jumps down a line instead. Not a huge deal right now, especially because I have a 24 inch screen, but I'll be looking into fixing that later when I have time.

--- scripts/os/deviceDriverFileSystem.js
+++ scripts/os/deviceDriverFileSystem.js
@@ -15,7 +15,13 @@ function DeviceDriverFileSystem()
     this.isr = null;
     
     //create a hard drive to be accessed
-    this.hardDrive = new hardDrive;
+   // this.hardDrive = new hardDrive;
+    
+    //sizes for t,s,b
+	this.numtracks = 4;
+	this.numsectors = 8;
+	this.numblocks = 8;
+	this.blocksize = 64;
     
     //FS methods
     this.format = krnFSFormat;
@@ -24,7 +30,8 @@ function DeviceDriverFileSystem()
     this.write = krnFSWrite;
     this.del = krnFSDelete; //javascript doesn't like me using reserved words :c
     this.ls = krnFSLS;
-    
+    this.createDisplay = krnFSCreateDisplay;
+    this.updateDisplay = krnFSUpdateDisplay;
     	
 }
 
@@ -37,7 +44,7 @@ function krnFSDriverEntry()
 	//you said you're going to format at the start anyway, but to ensure nothing
 	//breaks if/when testing is turned off...
 	
-	var initstatus = krnFSFormat();
+	var initstatus = krnFSFormat('inital');
 	
 	if (initstatus == false)
 		{
@@ -47,14 +54,100 @@ function krnFSDriverEntry()
     		krnTrace("File System loaded successfully!")
 		}
 	
+	
 	//not really sure what else to add here...
 }
 
-function krnFSFormat()
+function krnFSFormat(args)
 {
 	try
 	{
+		
 		localStorage.clear();
+		
+		var key;
+		var value;
+		var t = 0;
+		var s = 0;
+		var b = 0;
+		//init tracks
+		for(t=0;t<4;t++)
+		{
+			//krnTrace("TRACK");
+			//init sectors in each track
+			for(s=0;s<8;s++)
+			{
+				//krnTrace("SECTOR");
+				//init blocks in each sector
+				for(b=0;b<8;b++)
+				{
+					//krnTrace("BLOCK");
+					//make the tsb values into strings
+					t2 = t.toString();
+					s2 = s.toString();
+					b2 = b.toString();
+					
+					key = [t2, s2, b2];
+					
+					//create the data for values
+					var exists = 0;
+					var track = -1;
+					var sector = -1;
+					var block = -1;
+					//60 ` for now? might need to change this.
+					var data = "````````````````````````````````````````````````````````````";
+					
+					//turn the values into strings
+					var e = exists.toString();
+					var track2 = track.toString();
+					var sector2 = sector.toString();
+					var block2 = block.toString();
+					
+					value = [e, track2, sector2, block2, data];
+				
+					localStorage[key] = value;
+					//krnTrace("LOOK: " + localStorage[key]);
+					
+				}
+			
+				
+			}
+			
+		}
+		//create the data for mbr values
+		var mbr1 = 0;
+		var mbr2 = 0;
+		var mbr3 = 0;
+		
+		mbrt = t.toString();
+		mbrs = s.toString();
+		mbrb = b.toString();
+		
+		var mbr = [mbrt, mbrs, mbrb];
+		
+		var mbrexists = 1;
+		var mbrtrack = -1;
+		var mbrsector = -1;
+		var mbrblock = -1;
+		var data = "mbr";
+
+		//turn the values into strings
+		var mbre = mbrexists.toString();
+		var mbrtrack2 = mbrtrack.toString();
+		var mbrsector2 = mbrsector.toString();
+		var mbrblock2 = mbrblock.toString();
+
+		localStorage[mbr] = [mbre, mbrtrack2, mbrsector2, mbrblock2, data];
+
+		if (args.length > 0)
+			{
+			//krnTrace("GOING");
+			//this.createDisplay;
+			krnFSCreateDisplay();
+			}else{
+				this.updateDisplay;
+			}
+		
 		return true;
 	}
 	catch(e)
@@ -87,4 +180,63 @@ function krnFSLS()
 {
 	
 }
+
+function krnFSCreateDisplay()
+{
+	
+	var fsDisplay = document.getElementById("fileSystemDisplay");
+	fsDisplay.innerHTML = "<b> FILE SYSTEM </b>"; //clear it out
+	var rows = [];
+	var columns = [];
+	var i = 0;
+
+	var trackcount = 0;
+	var sectorcount = 0;
+	var blockcount = 0;
+	var line;
+
+	krnTrace("LEN: " + localStorage.length);
+	
+	var trackcount2 = trackcount.toString();
+	var sectorcount2 = sectorcount.toString();
+	var blockcount2 = blockcount.toString();
+	
+
+	
+	while (i <= localStorage.length)
+		{
+			keycount = [trackcount2, sectorcount2, blockcount2];
+			
+			var datdata = localStorage[keycount];
+			datdata = datdata.replace(/(\r\n|\n|\r)/gm,"");
+			//console.log(datdata);
+			
+			line = "[" + trackcount + ",     " + sectorcount + ",     " + blockcount + "]  :  " + datdata;
+			//console.log("LINE: " + line);
+			fsDisplay.innerHTML = fsDisplay.innerHTML + "<div>" + line + "</div>";
+			//fsDisplay.innerHTML = fsDisplay.innerHTML + "<div>" + "-"+ "</div>";
+			blockcount++;
+			if (blockcount > 7)
+				{
+					blockcount = 0;
+					sectorcount++;
+					if (sectorcount > 7)
+						{
+							sectorcount = 0;
+							trackcount++;
+						}
+				}
+			i++;
+		}
+
+	
+	
+}
+
+function krnFSUpdateDisplay()
+{
+
+	
+}
+
 
